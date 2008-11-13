910f56595ac8ee0b590267bfa0a7a9c9f6604486
Kevin Abel <kabel2@unl.edu>
2008-11-13 07:25:24 +0000

    Import from https://gforge-svn.unl.edu/unl-magento/

---
+++ app/code/local/Unl/Adminhtml/Block/Cms/Block/Edit/Form.php
@@ -0,0 +1,93 @@
+<?php
+class Unl_Adminhtml_Block_Cms_Block_Edit_Form extends Mage_Adminhtml_Block_Widget_Form
+{
+
+    /**
+     * Init form
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('block_form');
+        $this->setTitle(Mage::helper('cms')->__('Block Information'));
+    }
+
+    protected function _prepareForm()
+    {
+        $model = Mage::registry('cms_block');
+
+        $form = new Varien_Data_Form(array('id' => 'edit_form', 'action' => $this->getData('action'), 'method' => 'post'));
+
+        $form->setHtmlIdPrefix('block_');
+
+        $fieldset = $form->addFieldset('base_fieldset', array('legend'=>Mage::helper('cms')->__('General Information'), 'class' => 'fieldset-wide'));
+
+        if ($model->getBlockId()) {
+            $fieldset->addField('block_id', 'hidden', array(
+                'name' => 'block_id',
+            ));
+        }
+
+        $fieldset->addField('title', 'text', array(
+            'name'      => 'title',
+            'label'     => Mage::helper('cms')->__('Block Title'),
+            'title'     => Mage::helper('cms')->__('Block Title'),
+            'required'  => true,
+        ));
+
+        $fieldset->addField('identifier', 'text', array(
+            'name'      => 'identifier',
+            'label'     => Mage::helper('cms')->__('Identifier'),
+            'title'     => Mage::helper('cms')->__('Identifier'),
+            'required'  => true,
+            'class'     => 'validate-xml-identifier',
+        ));
+
+        /**
+         * Check is single store mode
+         */
+        if (!Mage::app()->isSingleStoreMode()) {
+            $fieldset->addField('store_id', 'multiselect', array(
+                'name'      => 'stores[]',
+                'label'     => Mage::helper('cms')->__('Store View'),
+                'title'     => Mage::helper('cms')->__('Store View'),
+                'required'  => true,
+                'values'    => Mage::getSingleton('adminhtml/system_store')->getStoreValuesForForm(false, true),
+            ));
+        }
+        else {
+            $fieldset->addField('store_id', 'hidden', array(
+                'name'      => 'stores[]',
+                'value'     => Mage::app()->getStore(true)->getId()
+            ));
+            $model->setStoreId(Mage::app()->getStore(true)->getId());
+        }
+
+        $fieldset->addField('is_active', 'select', array(
+            'label'     => Mage::helper('cms')->__('Status'),
+            'title'     => Mage::helper('cms')->__('Status'),
+            'name'      => 'is_active',
+            'required'  => true,
+            'options'   => array(
+                '1' => Mage::helper('cms')->__('Enabled'),
+                '0' => Mage::helper('cms')->__('Disabled'),
+            ),
+        ));
+
+        $fieldset->addField('content', 'editor', array(
+            'name'      => 'content',
+            'label'     => Mage::helper('cms')->__('Content'),
+            'title'     => Mage::helper('cms')->__('Content'),
+            'style'     => 'height:300px',
+            'wysiwyg'   => true,
+            'required'  => true,
+        ));
+
+        $form->setValues($model->getData());
+        $form->setUseContainer(true);
+        $this->setForm($form);
+
+        return parent::_prepareForm();
+    }
+
+}
---
+++ app/code/local/Unl/Adminhtml/Block/Cms/Page/Edit/Tab/Main.php
@@ -0,0 +1,82 @@
+<?php
+class Unl_Adminhtml_Block_Cms_Page_Edit_Tab_Main extends Mage_Adminhtml_Block_Widget_Form
+{
+
+    protected function _prepareForm()
+    {/** @var Cms_Model_Page */
+        $model = Mage::registry('cms_page');
+
+        $form = new Varien_Data_Form();
+
+        $form->setHtmlIdPrefix('page_');
+
+        $fieldset = $form->addFieldset('base_fieldset', array('legend'=>Mage::helper('cms')->__('General Information'),'class'=>'fieldset-wide'));
+
+        if ($model->getPageId()) {
+            $fieldset->addField('page_id', 'hidden', array(
+                'name' => 'page_id',
+            ));
+        }
+
+        $fieldset->addField('title', 'text', array(
+            'name'      => 'title',
+            'label'     => Mage::helper('cms')->__('Page Title'),
+            'title'     => Mage::helper('cms')->__('Page Title'),
+            'required'  => true,
+        ));
+
+        $fieldset->addField('identifier', 'text', array(
+            'name'      => 'identifier',
+            'label'     => Mage::helper('cms')->__('SEF URL Identifier'),
+            'title'     => Mage::helper('cms')->__('SEF URL Identifier'),
+            'required'  => true,
+            'class'     => 'validate-identifier',
+            'after_element_html' => '<p class="nm"><small>' . Mage::helper('cms')->__('(eg: domain.com/identifier)') . '</small></p>',
+        ));
+
+        /**
+         * Check is single store mode
+         */
+        if (!Mage::app()->isSingleStoreMode()) {
+            $fieldset->addField('store_id', 'multiselect', array(
+                'name'      => 'stores[]',
+                'label'     => Mage::helper('cms')->__('Store View'),
+                'title'     => Mage::helper('cms')->__('Store View'),
+                'required'  => true,
+                'values'    => Mage::getSingleton('adminhtml/system_store')->getStoreValuesForForm(false, true),
+            ));
+        }
+        else {
+            $fieldset->addField('store_id', 'hidden', array(
+                'name'      => 'stores[]',
+                'value'     => Mage::app()->getStore(true)->getId()
+            ));
+            $model->setStoreId(Mage::app()->getStore(true)->getId());
+        }
+
+        $fieldset->addField('is_active', 'select', array(
+            'label'     => Mage::helper('cms')->__('Status'),
+            'title'     => Mage::helper('cms')->__('Page Status'),
+            'name'      => 'is_active',
+            'required'  => true,
+            'options'   => array(
+                '1' => Mage::helper('cms')->__('Enabled'),
+                '0' => Mage::helper('cms')->__('Disabled'),
+            ),
+        ));
+
+        $fieldset->addField('content', 'editor', array(
+            'name'      => 'content',
+            'label'     => Mage::helper('cms')->__('Content'),
+            'title'     => Mage::helper('cms')->__('Content'),
+            'wysiwyg'   => true,
+            'required'  => true,
+        ));
+
+
+        $form->setValues($model->getData());
+        $this->setForm($form);
+
+        return parent::_prepareForm();
+    }
+}
