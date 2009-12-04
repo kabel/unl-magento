8bce2f50e41718880db52a1cd48f545931b21e0a
Kevin Abel <kabel2@unl.edu>
2009-12-04 00:05:36 +0000

    Admin interface changes to support "User Defined Qty" for checkbox bundle options

---
+++ app/code/local/Unl/Core/Block/Bundle/Adminhtml/Catalog/Product/Edit/Tab/Bundle/Option/Selection.php
@@ -0,0 +1,9 @@
+<?php
+
+class Unl_Core_Block_Bundle_Adminhtml_Catalog_Product_Edit_Tab_Bundle_Option_Selection extends Mage_Bundle_Block_Adminhtml_Catalog_Product_Edit_Tab_Bundle_Option_Selection
+{
+    public function __construct()
+    {
+        $this->setTemplate('unl/bundle/product/edit/bundle/option/selection.phtml');
+    }
+} 
