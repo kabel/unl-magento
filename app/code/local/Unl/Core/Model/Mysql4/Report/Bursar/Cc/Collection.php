35a2134a376d79b3e3d5feaf9bf085a77ff31ab3
Kevin Abel <kabel2@unl.edu>
2010-05-10 14:36:55 +0000

    * Move Delivery Date email elements to a block to avoid needing to nest {{depends}} directives

--- app/code/local/Unl/Core/Model/Mysql4/Report/Bursar/Cc/Collection.php
+++ app/code/local/Unl/Core/Model/Mysql4/Report/Bursar/Cc/Collection.php
@@ -2,5 +2,5 @@
 
 class Unl_Core_Model_Mysql4_Report_Bursar_Cc_Collection extends Unl_Core_Model_Mysql4_Report_Bursar_Collection_Abstract
 {
-    protected $_paymentMethodCodes = array('paypal_direct', 'paypal_express', 'verisign');
+    protected $_paymentMethodCodes = array('paypal_direct', 'paypal_express', 'paypaluk_direct', 'paypaluk_express', 'verisign');
 }
