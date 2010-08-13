c3bab9e775813b25be3ad331b63a61dbda4adfb8
Kevin Abel <kabel2@unl.edu>
2010-08-13 14:58:10 +0000

    * Patched a bug in the admin module that doesn't add a default store code to the URL when it is required for the router to work

--- app/code/local/Unl/Core/Block/Adminhtml/Sales/Order/Grid.php
+++ app/code/local/Unl/Core/Block/Adminhtml/Sales/Order/Grid.php
@@ -128,6 +128,24 @@ class Unl_Core_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtml_Block_Sal
         return Mage_Adminhtml_Block_Widget_Grid::_prepareColumns();
     }
     
+    /**
+     * Add new rss list to grid
+     *
+     * @param   string $url
+     * @param   string $label
+     * @return  Mage_Adminhtml_Block_Widget_Grid
+     */
+    public function addRssList($url, $label)
+    {
+        $this->_rssLists[] = new Varien_Object(
+            array(
+                'url'   => Mage::getModel('core/url')->getUrl($url, array('_store' => 'default')),
+                'label' => $label
+            )
+        );
+        return $this;
+    }
+    
     protected function _getPaymentMethods()
     {
         $config = Mage::getModel('payment/config');
