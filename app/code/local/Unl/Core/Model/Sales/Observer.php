4dc96acacf29d3435fd23a7db98980c971567179
Kevin Abel <kabel2@unl.edu>
2010-08-23 18:31:48 +0000

    * Updates to the Sales Observer * Added new event observer to help fix issues caused by MultiShip checkout items

--- app/code/local/Unl/Core/Model/Sales/Observer.php
+++ app/code/local/Unl/Core/Model/Sales/Observer.php
@@ -3,17 +3,44 @@
 
 class Unl_Core_Model_Sales_Observer
 {
-    public function setQuoteItemSourceStore($event)
+    /**
+     * Sets the quote_item's source store view 
+     * 
+     * @param $observer Varien_Event_Observer
+     */
+    public function setQuoteItemSourceStore($observer)
     {
-        /* @var $product Mage_Catalog_Model_Product */
-        $product = $event['product'];
-        /* @var $quote_item Mage_Sales_Model_Quote_Item */
-        $quote_item = $event['quote_item'];
+        $product = $observer->getEvent()->getProduct();
+        $quote_item = $observer->getEvent()->getQuoteItem();
         
         $sourceStore = $product->getSourceStoreView();
         
         if (null !== $sourceStore) {
             $quote_item->setSourceStoreView($sourceStore);
+        }
+    }
+    
+    
+    /**
+     * Adds some missing data members of quote_address_item to order_item from quote_item
+     * 
+     * @param $observer Varien_Event_Observer
+     */
+    public function onSalesConvertQuoteItemToOrderItem($observer)
+    {
+        $item = $observer->getEvent()->getItem();
+        
+        if ($item instanceof Mage_Sales_Model_Quote_Address_Item) {
+            $quoteItem = $item->getQuoteItem();
+            $orderItem = $observer->getEvent()->getOrderItem();
+            
+            $orderItem->setStoreId($quoteItem->getStoreId())
+                ->setSourceStoreView($quoteItem->getSourceStoreView());
+
+            //TODO: Fix tax_percent issue displayed in adminhtml
+//            if (null === $orderItem->getTaxPercent()) {
+//                
+//            }
         }
     }
 }
