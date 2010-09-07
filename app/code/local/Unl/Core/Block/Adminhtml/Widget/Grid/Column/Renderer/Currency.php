32df7576fc057c4a308350e196c773800db49a58
Kevin Abel <kabel2@unl.edu>
2010-09-07 21:39:51 +0000

    * Override the default renderExport method for the price and currency grid column renderers to allow excel to recognize the values as numbers rather than strings

---
+++ app/code/local/Unl/Core/Block/Adminhtml/Widget/Grid/Column/Renderer/Currency.php
@@ -0,0 +1,26 @@
+<?php
+
+class Unl_Core_Block_Adminhtml_Widget_Grid_Column_Renderer_Currency extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Currency
+{
+    /**
+     * Render column for export
+     *
+     * @param Varien_Object $row
+     * @return string
+     */
+    public function renderExport($row)
+    {
+        if ($data = $row->getData($this->getColumn()->getIndex())) {
+            $currency_code = $this->_getCurrencyCode($row);
+
+            if (!$currency_code) {
+                return $data;
+            }
+
+            $data = floatval($data) * $this->_getRate($row);
+            $data = sprintf("%f", $data);
+            return $data;
+        }
+        return $this->getColumn()->getDefault();
+    }
+}
