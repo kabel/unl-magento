d80369dc9bfeaa08f4294d9f2906d40a83e739c4
Kevin Abel <kabel2@unl.edu>
2010-10-25 15:29:55 +0000

    * Added the Delivery Date information to the shipment create form and view form and the packing slip PDF * Updated the shipment view form to ignore the Packages section if there are no packages

--- app/code/local/Unl/Core/Model/Sales/Order/Pdf/Shipment.php
+++ app/code/local/Unl/Core/Model/Sales/Order/Pdf/Shipment.php
@@ -30,10 +30,21 @@ class Unl_Core_Model_Sales_Order_Pdf_Shipment extends Unl_Core_Model_Sales_Order
             $this->insertAddress($page, $shipment->getStore());
 
             /* Add head */
-            $this->insertOrder($page, $order, Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_SHIPMENT_PUT_ORDER_ID, $order->getStoreId()));
-
+            $this->insertOrder($page, $order, Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_SHIPMENT_PUT_ORDER_ID, $order->getStoreId()));            
+            
             $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
             $this->_setFontRegular($page);
+            
+            if ($order->getAwDeliverydateDate()) {
+                $text = Mage::helper('deliverydate')->__('Delivery Date') . ': ' . Mage::helper('core')->formatDate($order->getAwDeliverydateDate(), 'medium', false);
+                if ($order->getAwDeliverydateNotice()) {
+                    $text .= "**";
+                }
+                $feed = self::DEFAULT_PAGE_MARGIN_RIGHT - self::DEFAULT_BOX_PAD - $this->widthForStringUsingFontSize($text, $page->getFont(), $page->getFontSize());
+                
+                $page->drawText($text, $feed, self::DEFAULT_PAGE_TOP - self::DEFAULT_LOGO_HEIGHT - self::DEFAULT_LOGO_MARGIN - self::DEFAULT_LINE_HEIGHT * 3, 'UTF-8');
+            }
+            
             $page->drawText(Mage::helper('sales')->__('Packingslip # ') . $shipment->getIncrementId(), self::DEFAULT_PAGE_LEFT, self::DEFAULT_PAGE_TOP - self::DEFAULT_LOGO_HEIGHT - self::DEFAULT_LOGO_MARGIN - self::DEFAULT_LINE_HEIGHT, 'UTF-8');
 
             /* Add table */
