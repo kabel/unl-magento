38d14fbd3d8b102807423b05d7ebbf15b254b381
Kevin Abel <kabel2@unl.edu>
2010-11-09 15:59:42 +0000

    * Fixed a bug in the refund process of bundle items with Qty > 1. Similar to invoice issue resolved earlier in same file.

--- app/code/local/Unl/Core/Model/Sales/Service/Order.php
+++ app/code/local/Unl/Core/Model/Sales/Service/Order.php
@@ -3,6 +3,47 @@
 class Unl_Core_Model_Sales_Service_Order extends Mage_Sales_Model_Service_Order
 {
     /**
+     * Prepare order creditmemo based on order items and requested params
+     *
+     * @param array $data
+     * @return Mage_Sales_Model_Order_Creditmemo
+     */
+    public function prepareCreditmemo($data = array())
+    {
+        $totalQty = 0;
+        $creditmemo = $this->_convertor->toCreditmemo($this->_order);
+        $qtys = isset($data['qtys']) ? $data['qtys'] : array();
+
+        foreach ($this->_order->getAllItems() as $orderItem) {
+            if (!$this->_canRefundItem($orderItem, $qtys)) {
+                continue;
+            }
+
+            $item = $this->_convertor->itemToCreditmemoItem($orderItem);
+            if ($orderItem->isDummy()) {
+                $qty = $orderItem->getQtyOrdered();
+            } else {
+                if (isset($qtys[$orderItem->getId()])) {
+                    $qty = (float) $qtys[$orderItem->getId()];
+                } elseif (!count($qtys)) {
+                    $qty = $orderItem->getQtyToRefund();
+                } else {
+                    continue;
+                }
+            }
+            $totalQty += $qty;
+            $item->setQty($qty);
+            $creditmemo->addItem($item);
+        }
+        $creditmemo->setTotalQty($totalQty);
+
+        $this->_initCreditmemoData($creditmemo, $data);
+
+        $creditmemo->collectTotals();
+        return $creditmemo;
+    }
+
+    /**
      * Prepare order invoice based on order data and requested items qtys
      *
      * @param array $data
