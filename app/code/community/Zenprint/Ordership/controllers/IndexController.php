34a0fade215e3599dce3c66dc35759374fcf829a
Kevin Abel <kabel2@unl.edu>
2010-11-09 16:56:57 +0000

    * Use the response object properly to avoid DEBUG messages about headers already being sent.

--- app/code/community/Zenprint/Ordership/controllers/IndexController.php
+++ app/code/community/Zenprint/Ordership/controllers/IndexController.php
@@ -17,59 +17,59 @@
  */
 
 class Zenprint_Ordership_IndexController extends Mage_Adminhtml_Controller_Action  {
-	
+
 	public function indexAction()  {
 		$this->loadLayout();
         $this->_setActiveMenu('sales');
-        
+
         $this->_addBreadcrumb($this->__('Sales'), $this->__('Sales'));
         $this->_addBreadcrumb($this->__('Orders'), $this->__('Orders'));
         $this->_addBreadcrumb($this->__('Order Ship'), $this->__('Order Ship'));
-        
+
         $this->_addContent($this->getLayout()->createBlock('adminhtml/ordership', 'ordership'));
         $this->renderLayout();
-		
+
 	}
-	
+
 	public function highvalueAction() {
 	   $pkgid = $this->getRequest()->getParam('id');
         if(empty($pkgid))  {
             echo "Invalid package id";
             exit();
         }
-        
+
         $pkg = Mage::getModel('shipping/shipment_package')->load($pkgid);
         $img = $pkg->getInsDoc();
-        
+
         if (empty($img)) {
             echo "Invalid package id";
             exit();
         }
-        
-        echo base64_decode($img);
+
+        $this->getResponse()->setBody(base64_decode($img));
 	}
-	
+
     public function customsAction() {
        $pkgid = $this->getRequest()->getParam('id');
         if(empty($pkgid))  {
             echo "Invalid package id";
             exit();
         }
-        
+
         $pkg = Mage::getModel('shipping/shipment_package')->load($pkgid);
         $img = $pkg->getIntlDoc();
-        
+
         if (empty($img)) {
             echo "Invalid package id";
             exit();
         }
-        
-        header('Content-type: application/pdf');
-        echo base64_decode($img);
+
+        $this->getResponse()->setHeader('Content-type', 'application/pdf');
+        $this->getResponse()->setBody(base64_decode($img));
     }
-	
+
 	/**
-	 * Shows the shipping label image for the specified package. 
+	 * Shows the shipping label image for the specified package.
 	 */
 	public function labelAction()  {
 		$pkgid = $this->getRequest()->getParam('id');
@@ -77,53 +77,53 @@ class Zenprint_Ordership_IndexController extends Mage_Adminhtml_Controller_Actio
 			echo "Invalid package id";
 			exit();
 		}
-		
+
 		//get image details
 		$pkg = Mage::getModel('shipping/shipment_package')->load($pkgid);
 		$imgtype = strtolower($pkg->getLabelFormat());
 		$img = $pkg->getLabelImage();
-		
+
 		if(empty($imgtype) || empty($img))  {
 			echo "Invalid package id";
 			exit();
 		}
-		
+
 		//output image
-		header('Content-type: '.$imgtype);
-		echo base64_decode($img);
+		$this->getResponse()->setHeader('Content-type', $imgtype);
+		$this->getResponse()->setBody(base64_decode($img));
 	}
-	
+
 	/**
 	 * Shows the shipping label PDF so it can be printed for the specified package(s).
 	 */
 	public function labelPdfAction()  {
-		
+
 		//label size in a pts (1/72 inch) - 6" x 4"
 		$label4x6landscape = '432:288:';
 		$label4x6portrait = '288:432:';
-		
-		$packageids = explode('|', $this->getRequest()->getParam('id'));		
+
+		$packageids = explode('|', $this->getRequest()->getParam('id'));
 		if(empty($packageids))  {
-			echo "Invalid package id."; 
+			echo "Invalid package id.";
 			exit();
 		}
 
 		//create a PDF of the label images to be printed
 		$pdf = new Zend_Pdf();
-		
+
 		//add new page for each package id
 		foreach ($packageids as $pkgid)  {
 			//retrieve the package object
 			$pkg = Mage::getModel('shipping/shipment_package')->load($pkgid);
 			$imgformat = $pkg->getLabelFormat();
 			$imgstr = $pkg->getLabelImage();
-			
+
 			//if id is invalid return error
 			if(empty($imgformat) || empty($imgstr))  {
-				echo "Invalid package id."; 
+				echo "Invalid package id.";
 				exit();
 			}
-			
+
 			//get the image in the proper format
 			$pngimagepath = $pkg->getLabelImageAsPng();
 
@@ -141,26 +141,24 @@ class Zenprint_Ordership_IndexController extends Mage_Adminhtml_Controller_Actio
 				}
 			}
 			$pdfpage = $pdf->newPage($layout);
-						
+
 			//get the image into a PdfImage object
 			$pdfimg = Zend_Pdf_Image::imageWithPath($pngimagepath);  //jpg, png, tiff images supported
 			$pdfpage->drawImage($pdfimg, 0, 0, $pdfpage->getWidth(), $pdfpage->getHeight());
-			
+
 			$pdf->pages[] = $pdfpage;
-			
+
 			//clean up the pngimagepath
 			unlink($pngimagepath);
 		}
-		
+
 		//output the PDF
-		header('Content-type: application/pdf');
-		echo $pdf->render();
-		
+		$this->getResponse()->setHeader('Content-type', 'application/pdf');
+		$this->getResponse()->setBody($pdf->render());
 	}
-	
+
 	protected function _isAllowed()
     {
         return Mage::getSingleton('admin/session')->isAllowed('sales/order/actions/ship');
     }
 }
-?>
