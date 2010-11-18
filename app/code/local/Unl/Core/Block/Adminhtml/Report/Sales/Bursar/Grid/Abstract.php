bee01b1e3c757be621791830962349535e9e2944
Kevin Abel <kabel2@unl.edu>
2010-11-18 20:09:14 +0000

    * Fixed undocumented change in the grouped column Grid member name

--- app/code/local/Unl/Core/Block/Adminhtml/Report/Sales/Bursar/Grid/Abstract.php
+++ app/code/local/Unl/Core/Block/Adminhtml/Report/Sales/Bursar/Grid/Abstract.php
@@ -2,15 +2,15 @@
 
 class Unl_Core_Block_Adminhtml_Report_Sales_Bursar_Grid_Abstract extends Mage_Adminhtml_Block_Report_Grid_Abstract
 {
-    protected $_columnGroupBy = 'period';
-    
+    protected $_groupedColumn = array('period');
+
     public function __construct()
     {
         parent::__construct();
         $this->setCountTotals(true);
         $this->setCountSubTotals(true);
     }
-    
+
     public function _exportIterateCollection($callback, array $args)
     {
         // Overrides the paging because UNION queries can't be paged
--- app/code/local/Unl/Core/Block/Adminhtml/Report/Sales/Reconcile/Grid/Abstract.php
+++ app/code/local/Unl/Core/Block/Adminhtml/Report/Sales/Reconcile/Grid/Abstract.php
@@ -2,8 +2,8 @@
 
 class Unl_Core_Block_Adminhtml_Report_Sales_Reconcile_Grid_Abstract extends Mage_Adminhtml_Block_Report_Grid_Abstract
 {
-    protected $_columnGroupBy = 'period';
-    
+    protected $_groupedColumn = array('period');
+
     public function __construct()
     {
         parent::__construct();
