9f45a958624016e117e7822c9f4ff5604f590417
Kevin Abel <kabel2@unl.edu>
2011-02-11 20:20:40 +0000

    Added a new grid block that is capabile of rendering multiple grids on the same page. To be used with the new Bursar/Reconcile reports to get rid of the need for union queries.

---
+++ app/code/local/Unl/Core/Block/Adminhtml/Widget/Grid/Multicontainer.php
@@ -0,0 +1,89 @@
+<?php
+
+class Unl_Core_Block_Adminhtml_Widget_Grid_Multicontainer extends Mage_Adminhtml_Block_Widget_Container
+{
+    protected $_grids = array();
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->setTemplate('report/grid/multicontainer.phtml');
+    }
+
+    protected function _prepareLayout()
+    {
+        foreach ($this->_grids as $id => $data) {
+            $childId = $this->_prepareGridBlockId($id);
+            $this->_addGridChildBlock($childId, $data['block_type']);
+        }
+        return parent::_prepareLayout();
+    }
+
+    public function addGrid($id, $block, $data = array())
+    {
+        $this->_grids[$id] = $data;
+        $this->_grids[$id]['block_type'] = $block;
+
+        return $this;
+    }
+
+    public function removeGrid($id)
+    {
+        if (isset($this->_grids[$id])) {
+            unset($this->_grids[$id]);
+        }
+
+        return $this;
+    }
+
+    protected function _prepareGridBlockId($id)
+    {
+        return $id . '_grid';
+    }
+
+    protected function _addGridChildBlock($childId, $blockType)
+    {
+        $block = $this->getLayout()->createBlock($blockType);
+        $block->setTemplate('widget/grid/multigrid.phtml');
+        $this->setChild($childId, $block);
+        return $block;
+    }
+
+    public function getGridsHtml()
+    {
+        $out = '';
+
+        foreach ($this->_grids as $id => $data) {
+            $childId = $this->_prepareGridBlockId($id);
+            $child = $this->getChild($childId);
+
+            if (!$child) {
+                $child = $this->_addGridChildBlock($childId, $data['block_type']);
+            }
+            unset($data['block_type']);
+
+            $child->setData($data);
+
+            foreach (array('filter_data', 'period_type') as $key) {
+                if ($this->hasData($key)) {
+                    $child->setData($key, $this->getData($key));
+                }
+            }
+
+            $out .= $this->getChildHtml($childId);
+        }
+
+        return $out;
+    }
+
+    public function getHeaderCssClass()
+    {
+        return 'icon-head ' . parent::getHeaderCssClass();
+    }
+
+    public function getHeaderWidth()
+    {
+        return 'width:50%;';
+    }
+}
