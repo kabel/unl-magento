cfdb8e086284d23db3824455e4f584cdc7bdc7dc
Kevin Abel <kabel2@unl.edu>
2011-03-11 21:53:26 +0000

    Added a common Xmlship interface for shipping carriers that support label generation.

---
+++ app/code/local/Unl/Ship/Model/Shipping/Carrier/Xmlship/Interface.php
@@ -0,0 +1,20 @@
+<?php
+
+interface Unl_Ship_Model_Shipping_Carrier_Xmlship_Interface  {
+
+	/**
+     * Creates a shipment to be sent. Should initialize the shipment, retrieve tracking number, and get shipping label.
+     *
+     * @return array An array of Mage_Shipping_Model_Shipment_Package objects
+     * @throws Mage_Shipping_Exception
+     */
+    public function createShipment(Unl_Ship_Model_Shipment_Request $request);
+
+	public function getCode($type, $code='');
+
+	public function addErrorRetry($code);
+
+	public function isErrorRetried($code);
+
+	public function isRequestRetryAble($code);
+}
