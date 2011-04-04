ca193fa27b765a5ec68f0a066276e65eb3b79e81
Kevin Abel <kabel2@unl.edu>
2011-04-04 22:15:24 +0000

    Refactored common colleciton functions into the collection models.

--- app/code/local/Unl/Inventory/Model/Mysql4/Audit/Collection.php
+++ app/code/local/Unl/Inventory/Model/Mysql4/Audit/Collection.php
@@ -6,4 +6,33 @@ class Unl_Inventory_Model_Mysql4_Audit_Collection extends Mage_Core_Model_Mysql4
 	{
 		$this->_init('unl_inventory/audit');
 	}
+
+	public function addProductFilter($product)
+	{
+	    $this->addFieldToFilter('product_id', $product->getId());
+
+	    return $this;
+	}
+
+	public function addCostPerItem()
+	{
+	    $this->addExpressionFieldToSelect('cost_per_item', 'IF({{amount}}, {{amount}} / {{qty}}, NULL)',
+	        array('amount' => 'amount', 'qty' => 'qty'));
+
+	    return $this;
+	}
+
+	/**
+	 * Get the select statement to show audited products
+	 *
+	 * @return Varien_Db_Select
+	 */
+	public function getProductSelect()
+	{
+	    $select = $this->getSelect()->reset(Zend_Db_Select::COLUMNS);
+	    $select->columns(array('product_id'))
+	        ->group('product_id');
+
+	    return $select;
+	}
 }
--- app/code/local/Unl/Inventory/Model/Mysql4/Index/Collection.php
+++ app/code/local/Unl/Inventory/Model/Mysql4/Index/Collection.php
@@ -6,4 +6,19 @@ class Unl_Inventory_Model_Mysql4_Index_Collection extends Mage_Core_Model_Mysql4
 	{
 		$this->_init('unl_inventory/index');
 	}
+
+
+	/**
+	 * Get the select statement used to show the current qty_on_hand for a product
+	 *
+	 * @return Varien_Db_Select
+	 */
+	public function getQtyOnHandSelect()
+	{
+	    $select = $this->getSelect()->reset(Zend_Db_Select::COLUMNS);
+	    $select->columns(array('qty' => 'SUM(qty_on_hand)', 'product_id'))
+	        ->group('product_id');
+
+	    return $select;
+	}
 }
