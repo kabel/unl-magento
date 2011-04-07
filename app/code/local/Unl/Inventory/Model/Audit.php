0858905cf4c1cf5944c4c09a6940c971a834b6c5
Kevin Abel <kabel2@unl.edu>
2011-04-07 18:06:52 +0000

    Added a new audit type to allow system notes to be saved in the audit log.

--- app/code/local/Unl/Inventory/Model/Audit.php
+++ app/code/local/Unl/Inventory/Model/Audit.php
@@ -6,6 +6,7 @@ class Unl_Inventory_Model_Audit extends Mage_Core_Model_Abstract
     const TYPE_CREDIT      = 2;
     const TYPE_PURCHASE    = 3;
     const TYPE_ADJUSTMENT  = 4;
+    const TYPE_NOTE_ONLY   = 5;
 
     const TYPE_ADJUSTMENT_SET     = 1;
     const TYPE_ADJUSTMENT_OFFSET  = 2;
