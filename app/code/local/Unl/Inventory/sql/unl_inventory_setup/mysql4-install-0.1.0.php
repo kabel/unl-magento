ef1fbbcd172935e30a1df777cd3d888310640237
Kevin Abel <kabel2@unl.edu>
2011-04-07 18:05:57 +0000

    Add new index tables that will assist in the reindex and join processes to get all product's quickly.

--- app/code/local/Unl/Inventory/sql/unl_inventory_setup/mysql4-install-0.1.0.php
+++ app/code/local/Unl/Inventory/sql/unl_inventory_setup/mysql4-install-0.1.0.php
@@ -6,7 +6,6 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-
 -- DROP TABLE IF EXISTS `{$this->getTable('unl_inventory/audit')}`;
 CREATE TABLE `{$this->getTable('unl_inventory/audit')}` (
     `audit_id` int(10) unsigned NOT NULL auto_increment,
@@ -33,6 +32,23 @@ CREATE TABLE `{$this->getTable('unl_inventory/index')}` (
     CONSTRAINT `FK_UNL_INVENTORY_INDEX_PRODUCT` FOREIGN KEY (`product_id`) REFERENCES `{$this->getTable('catalog_product_entity')}` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- DROP TABLE IF EXISTS `{$this->getTable('unl_inventory/index_idx')}`;
+CREATE TABLE `{$this->getTable('unl_inventory/index_idx')}` (
+    `index_id` int(10) unsigned NOT NULL,
+    `product_id` int(10) unsigned NOT NULL,
+    PRIMARY KEY  (`product_id`),
+    KEY `FK_UNL_INVENTORY_INDEX_IDX_INDEX` (`index_id`),
+    CONSTRAINT `FK_UNL_INVENTORY_INDEX_IDX_INDEX` FOREIGN KEY (`index_id`) REFERENCES `{$this->getTable('unl_inventory/index')}` (`index_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- DROP TABLE IF EXISTS `{$this->getTable('unl_inventory/index_tmp')}`;
+CREATE TABLE `{$installer->getTable('unl_inventory/index_tmp')}` (
+     `product_id` int(10) unsigned NOT NULL,
+     `qty_on_hand` decimal(12,4) NOT NULL default '0.0000',
+     `amount` decimal(12,4) NOT NULL default '0.0000',
+     `created_at` DATETIME NOT NULL default '0000-00-00',
+     PRIMARY KEY  (`product_id`)
+) ENGINE=MEMORY DEFAULT CHARSET=utf8;
 ");
 
 $setup = new Mage_Catalog_Model_Resource_Eav_Mysql4_Setup('catalog_setup');
