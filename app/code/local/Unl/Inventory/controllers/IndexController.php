302b663f838b7d108c18f318e2d29382b1869964
Kevin Abel <kabel2@unl.edu>
2011-04-08 00:27:35 +0000

    Implemented the save controller action for the edit inventory interface.

--- app/code/local/Unl/Inventory/controllers/IndexController.php
+++ app/code/local/Unl/Inventory/controllers/IndexController.php
@@ -42,6 +42,10 @@ class Unl_Inventory_IndexController extends Mage_Adminhtml_Controller_Action
             return $this->_redirect('*/*/');
         }
 
+        // set entered data if was error when we do save
+        $data = Mage::getSingleton('adminhtml/session')->getInventoryAdjustmentData(true);
+        $product->setAdjustmentFormData($data);
+
         $this->loadLayout();
         $this->_title($product->getName());
         $this->_setActiveMenu('catalog/inventory');
@@ -50,7 +54,51 @@ class Unl_Inventory_IndexController extends Mage_Adminhtml_Controller_Action
 
     public function saveAction()
     {
+        if (!$product = $this->_initProduct()) {
+            return $this->_redirect('*/*/');
+        }
 
+        $data = $this->getRequest()->getPost();
+        if (empty($data) || $data['id'] != $product->getId()) {
+            return $this->_redirect('*/*/');
+        }
+
+        $redirectBack   = $this->getRequest()->getParam('back', false);
+
+        try {
+            if (!Mage::helper('unl_inventory')->getIsAuditInventory($product)) {
+                Mage::throwException($this->__("This product's inventory is currently not being audited. Save failed."));
+            }
+
+            $auditLog = Mage::getModel('unl_inventory/audit');
+            $auditLog->setData($data['adjust']);
+            $auditLog->setProduct($product);
+            $auditLog->setProductId($product->getId());
+
+            $result = $auditLog->validate();
+            if (is_string($result)) {
+                Mage::throwException($result);
+            }
+
+            $auditLog->setRegisterFlag(true);
+            $auditLog->setCreatedAt(now());
+            $auditLog->save();
+
+            Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('unl_inventory')->__('The inventory update has been logged.'));
+
+            if ($redirectBack) {
+                return $this->_redirect('*/*/edit', array(
+                    'id'    => $product->getId(),
+                    '_current'=>true
+                ));
+            }
+        } catch (Exception $e) {
+            Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            Mage::getSingleton('adminhtml/session')->setInventoryAdjustmentData(isset($data['adjust']) ? $data['adjust'] : array());
+            return $this->_redirect('*/*/edit', array('id'=>$product->getId(), '_current'=>true));
+        }
+
+        $this->_redirect('*/*/');
     }
 
     public function auditGridAction()
@@ -61,12 +109,12 @@ class Unl_Inventory_IndexController extends Mage_Adminhtml_Controller_Action
 
     public function exportAuditCsvAction()
     {
-
+        //TODO: Implement Export
     }
 
     public function exportAuditExcelAction()
     {
-
+        //TODO: Implement Export
     }
 
     protected function _isAllowed()
