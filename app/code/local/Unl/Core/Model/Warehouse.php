f5ed77ae9ecb3b333f6fa011ab8d12a494b58374
Kevin Abel <kabel2@unl.edu>
2011-08-04 18:23:22 +0000

    Removed the PENDING_PAYMENT order state from the list of filtered warehouse states.

--- app/code/local/Unl/Core/Model/Warehouse.php
+++ app/code/local/Unl/Core/Model/Warehouse.php
@@ -6,7 +6,6 @@ class Unl_Core_Model_Warehouse  extends Mage_Core_Model_Abstract
         Mage_Sales_Model_Order::STATE_NEW,
         Mage_Sales_Model_Order::STATE_HOLDED,
         Mage_Sales_Model_Order::STATE_PAYMENT_REVIEW,
-        Mage_Sales_Model_Order::STATE_PENDING_PAYMENT,
     );
 
 	protected function _construct()
