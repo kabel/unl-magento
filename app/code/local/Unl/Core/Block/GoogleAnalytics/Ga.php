a98c3da6a250f9c068e6144b33e5e4d82aa6c729
Kevin Abel <kabel2@unl.edu>
2011-08-10 22:03:20 +0000

    Updated the GA code to support the new page load time tracking.

--- app/code/local/Unl/Core/Block/GoogleAnalytics/Ga.php
+++ app/code/local/Unl/Core/Block/GoogleAnalytics/Ga.php
@@ -3,6 +3,7 @@
 class Unl_Core_Block_GoogleAnalytics_Ga extends Mage_GoogleAnalytics_Block_Ga
 {
     const XML_PATH_OUTPUT_SCRIPT = 'google/analytics/output_script';
+    const XML_PATH_TRACK_PAGE_LOAD = 'google/analytics/track_page_load';
 
     /* Overrides
      * @see Mage_GoogleAnalytics_Block_Ga::_toHtml()
@@ -38,5 +39,16 @@ class Unl_Core_Block_GoogleAnalytics_Ga extends Mage_GoogleAnalytics_Block_Ga
         ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
         (document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(ga);
     })();' . "\n";
+    }
+
+    protected function _getPageLoadTrackingCode()
+    {
+        if (!Mage::getStoreConfigFlag(self::XML_PATH_TRACK_PAGE_LOAD)) {
+            return '';
+        }
+
+        return "
+_gaq.push(['_trackPageLoadTime']);
+";
     }
 }
