e759c1dbba29222a882c4efeedb67df80e9da567
Kevin Abel <kabel2@unl.edu>
2011-12-14 21:28:59 +0000

    Added order state logic for write off invoice orders.

--- app/code/local/Unl/Core/Model/Sales/Order.php
+++ app/code/local/Unl/Core/Model/Sales/Order.php
@@ -2,6 +2,54 @@
 
 class Unl_Core_Model_Sales_Order extends Mage_Sales_Model_Order
 {
+    /* Overrides
+     * @see Mage_Sales_Model_Order::_checkState()
+     * by adding state logic for orders with invoices that are write offs
+     */
+    protected function _checkState()
+    {
+        if (!$this->getId()) {
+            return $this;
+        }
+
+        $userNotification = $this->hasCustomerNoteNotify() ? $this->getCustomerNoteNotify() : null;
+
+        if (!$this->isCanceled()
+            && !$this->canUnhold()
+            && !$this->canInvoice()) {
+            foreach ($this->getInvoiceCollection() as $invoice) {
+                if ($invoice->getState() == Unl_Core_Model_Sales_Order_Invoice::STATE_WRITEOFF) {
+                    if ($this->getState() !== self::STATE_CLOSED) {
+                        $this->_setState(self::STATE_CLOSED, true, '', $userNotification);
+                        return $this;
+                    }
+                }
+            }
+
+            if (!$this->canShip()) {
+                if (0 == $this->getBaseGrandTotal() || $this->canCreditmemo()) {
+                    if ($this->getState() !== self::STATE_COMPLETE) {
+                        $this->_setState(self::STATE_COMPLETE, true, '', $userNotification);
+                    }
+                }
+                /**
+                 * Order can be closed just in case when we have refunded amount.
+                 * In case of "0" grand total order checking ForcedCanCreditmemo flag
+                 */
+                elseif(floatval($this->getTotalRefunded()) || (!$this->getTotalRefunded() && $this->hasForcedCanCreditmemo())) {
+                    if ($this->getState() !== self::STATE_CLOSED) {
+                        $this->_setState(self::STATE_CLOSED, true, '', $userNotification);
+                    }
+                }
+            }
+        }
+
+        if ($this->getState() == self::STATE_NEW && $this->getIsInProcess()) {
+            $this->setState(self::STATE_PROCESSING, true, '', $userNotification);
+        }
+        return $this;
+    }
+
     protected function _getEmails($configPath)
     {
         $storeIds = array();
