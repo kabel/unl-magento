b2eb2ff5d61723c6785cb59a2c3d8f7fb5c8a07b
Kevin Abel <kabel2@unl.edu>
2012-03-31 06:07:41 +0000

    Move the payment account resource models into new resource folder.

--- app/code/local/Unl/Payment/Model/Resource/Account.php
+++ app/code/local/Unl/Payment/Model/Resource/Account.php
@@ -1,6 +1,6 @@
 <?php
 
-class Unl_Payment_Model_Mysql4_Account extends Mage_Core_Model_Mysql4_Abstract
+class Unl_Payment_Model_Resource_Account extends Mage_Core_Model_Resource_Db_Abstract
 {
     protected function _construct()
     {
--- app/code/local/Unl/Payment/Model/Resource/Account/Collection.php
+++ app/code/local/Unl/Payment/Model/Resource/Account/Collection.php
@@ -1,6 +1,6 @@
 <?php
 
-class Unl_Payment_Model_Mysql4_Account_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
+class Unl_Payment_Model_Resource_Account_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
 {
     protected function _construct()
     {
