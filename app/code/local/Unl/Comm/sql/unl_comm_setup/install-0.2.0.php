b4621f637a5fc1a6be8fd7145d070efe822ca677
Kevin Abel <kabel2@unl.edu>
2012-03-31 06:29:57 +0000

    Update comm module config and sql to Magento 1.6.2

---
+++ app/code/local/Unl/Comm/sql/unl_comm_setup/install-0.2.0.php
@@ -0,0 +1,98 @@
+<?php
+
+/* @var $installer Unl_Comm_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+
+/**
+ * Create unl_comm/queue table
+ */
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('unl_comm/queue'))
+    ->addColumn('queue_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'unsigned'  => true,
+            'nullable'  => false,
+            'primary'   => true,
+            'identity'  => true,
+        ),
+        'Queue Id'
+    )
+    ->addColumn('queue_status', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+            'unsigned'  => true,
+            'nullable'  => false,
+            'default'   => '0',
+        ),
+        'Queue Status'
+    )
+    ->addColumn('queue_start_at', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(),
+        'Start Date'
+    )
+    ->addColumn('queue_finish_at', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(),
+        'Finished Date'
+    )
+    ->addColumn('message_type', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+            'unsigned'  => true,
+        ),
+        'Message Type'
+    )
+    ->addColumn('message_text', Varien_Db_Ddl_Table::TYPE_TEXT, '2M', array(),
+        'Message Text'
+    )
+    ->addColumn('message_styles', Varien_Db_Ddl_Table::TYPE_TEXT, '1M', array(),
+        'Message Styles'
+    )
+    ->addColumn('message_subject', Varien_Db_Ddl_Table::TYPE_TEXT, 300, array(),
+        'Message Subject'
+    )
+    ->addColumn('message_sender_name', Varien_Db_Ddl_Table::TYPE_TEXT, 200, array(),
+        'Message Sender Name'
+    )
+    ->addColumn('message_sender_email', Varien_Db_Ddl_Table::TYPE_TEXT, 200, array(),
+        'Message Sender Email'
+    )
+    ->setComment('Communication Queue');
+$installer->getConnection()->createTable($table);
+
+/**
+ * Create unl_comm/queue_link table
+ */
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('unl_comm/queue_link'))
+    ->addColumn('queue_link_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'unsigned'  => true,
+            'nullable'  => false,
+            'primary'   => true,
+            'identity'  => true,
+        ),
+        'Queue Link Id'
+    )
+    ->addColumn('queue_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'unsigned'  => true,
+            'nullable'  => false,
+            'default'   => '0',
+         ),
+        'Queue Id'
+    )
+    ->addColumn('customer_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'unsigned'  => true,
+            'nullable'  => false,
+            'default'   => '0',
+        ),
+        'Customer Id'
+    )
+    ->addColumn('sent_at', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(),
+        'Sent At'
+    )
+    ->addIndex($installer->getIdxName($installer->getTable('unl_comm/queue_link'), array('sent_at')), array('sent_at'))
+    ->addForeignKey($installer->getFkName('unl_comm/queue_link', 'queue_id', 'unl_comm/queue', 'queue_id'),
+        'queue_id', $installer->getTable('unl_comm/queue'), 'queue_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE
+    )
+    ->addForeignKey($installer->getFkName('unl_comm/queue_link', 'customer_id', 'customer/entity', 'entity_id'),
+        'customer_id', $installer->getTable('customer/entity'), 'entity_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE
+    )
+    ->setComment('Communication Queue Recipient');
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
---
+++ app/code/local/Unl/Comm/sql/unl_comm_setup/mysql4-upgrade-0.1.0-0.2.0.php
@@ -0,0 +1,152 @@
+<?php
+
+/* @var $installer Unl_Comm_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+
+/**
+ * Drop foreign keys
+ */
+$installer->getConnection()->dropForeignKey(
+    $installer->getTable('unl_comm/queue_link'),
+    'FK_COMM_QUEUE_LINK_QUEUE'
+);
+
+$installer->getConnection()->dropForeignKey(
+    $installer->getTable('unl_comm/queue_link'),
+    'FK_COMM_QUEUE_LINK_CUSTOMER'
+);
+
+/**
+ * Drop indexes
+ */
+$installer->getConnection()->dropIndex(
+    $installer->getTable('unl_comm/queue_link'),
+    'IDX_COMM_QUEUE_LINK_SENT_AT'
+);
+
+/**
+ * Change columns
+ */
+$tables = array(
+    $installer->getTable('unl_comm/queue') => array(
+        'columns' => array(
+            'queue_id' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_INTEGER,
+                'unsigned'  => true,
+                'nullable'  => false,
+                'primary'   => true,
+                'identity'  => true,
+                'comment'   => 'Queue Id'
+            ),
+            'queue_status' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_SMALLINT,
+                'unsigned'  => true,
+                'nullable'  => false,
+                'default'   => '0',
+                'comment'   => 'Queue Status'
+            ),
+            'queue_start_at' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_DATETIME,
+                'comment'   => 'Start Date'
+            ),
+            'queue_finish_at' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_DATETIME,
+                'comment'   => 'Finished Date'
+            ),
+            'message_type' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_SMALLINT,
+                'unsigned'  => true,
+                'comment'   => 'Message Type'
+            ),
+            'message_text' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => '2M',
+                'comment'   => 'Message Text'
+            ),
+            'message_styles' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => '1M',
+                'comment'   => 'Message Styles'
+            ),
+            'message_subject' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 300,
+                'comment'   => 'Message Subject'
+            ),
+            'message_sender_name' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 200,
+                'comment'   => 'Message Sender Name'
+            ),
+            'message_sender_email' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+                'length'    => 200,
+                'comment'   => 'Message Sender Email'
+            )
+        ),
+        'comment' => 'Communication Queue'
+    ),
+    $installer->getTable('unl_comm/queue_link') => array(
+        'columns' => array(
+            'queue_link_id' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_INTEGER,
+                'unsigned'  => true,
+                'nullable'  => false,
+                'primary'   => true,
+                'identity'  => true,
+                'comment'   => 'Queue Link Id'
+            ),
+            'queue_id' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_INTEGER,
+                'unsigned'  => true,
+                'nullable'  => false,
+                'default'   => '0',
+                'comment'   => 'Queue Id'
+            ),
+            'customer_id' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_INTEGER,
+                'unsigned'  => true,
+                'nullable'  => false,
+                'default'   => '0',
+                'comment'   => 'Customer Id'
+            ),
+            'sent_at' => array(
+                'type'      => Varien_Db_Ddl_Table::TYPE_DATETIME,
+                'comment'   => 'Sent At'
+            )
+        ),
+        'comment' => 'Communication Queue Recipient'
+    )
+);
+$installer->getConnection()->modifyTables($tables);
+
+/**
+ * Add indexes
+ */
+$installer->getConnection()->addIndex(
+    $installer->getTable('unl_comm/queue_link'),
+    $installer->getIdxName('unl_comm/queue_link', array('sent_at')),
+    array('sent_at')
+);
+
+/**
+ * Add foreign keys
+ */
+$installer->getConnection()->addForeignKey(
+    $installer->getFkName('unl_comm/queue_link', 'queue_id', 'unl_comm/queue', 'queue_id'),
+    $installer->getTable('unl_comm/queue_link'),
+    'queue_id',
+    $installer->getTable('unl_comm/queue'),
+    'queue_id'
+);
+
+$installer->getConnection()->addForeignKey(
+    $installer->getFkName('unl_comm/queue_link', 'customer_id', 'customer/entity', 'entity_id'),
+    $installer->getTable('unl_comm/queue_link'),
+    'customer_id',
+    $installer->getTable('customer/entity'),
+    'entity_id'
+);
+
+$installer->endSetup();
