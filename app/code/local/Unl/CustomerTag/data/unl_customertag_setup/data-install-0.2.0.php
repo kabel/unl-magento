8338413ca2b41b0586b2d52efa2a5bd92b593bd7
Kevin Abel <kabel2@unl.edu>
2012-03-31 06:23:31 +0000

    Add data installer for customer tag module.

---
+++ app/code/local/Unl/CustomerTag/data/unl_customertag_setup/data-install-0.2.0.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $installer Unl_CustomerTag_Model_Resource_Setup */
+$installer = $this;
+
+/**
+ * install default customer tag
+ */
+$data = array(
+    'name'        => 'Allow Invoicing',
+    'is_system'   => 1
+);
+$installer->getConnection()->insert($installer->getTable('unl_customertag/tag'), $data);
