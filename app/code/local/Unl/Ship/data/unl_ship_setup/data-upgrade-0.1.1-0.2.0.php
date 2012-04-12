7ac48f8013be4f66906023039938abcde462abb1
Kevin Abel <kabel2@unl.edu>
2012-04-12 02:41:05 +0000

    Add a data update script that will convert old base64 encoded data in unl_shipment_package to unencoded binary.

---
+++ app/code/local/Unl/Ship/data/unl_ship_setup/data-upgrade-0.1.1-0.2.0.php
@@ -0,0 +1,28 @@
+<?php
+
+/* @var $installer Unl_Ship_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+
+/* @var $pkgs Unl_Ship_Model_Resource_Shipment_Package_Collection */
+$pkgs = Mage::getModel('unl_ship/shipment_package')->getCollection();
+
+while ($pkg = $pkgs->fetchItem()) {
+    $pkg->setLabelImage(base64_decode($pkg->getLabelImage()));
+
+    if ($pkg->getHtmlLabelImage()) {
+        $pkg->setHtmlLabelImage(base64_decode($pkg->getHtmlLabelImage()));
+    }
+
+    if ($pkg->getInsDoc()) {
+        $pkg->setInsDoc(base64_decode($pkg->getInsDoc()));
+    }
+
+    if ($pkg->getIntlDoc()) {
+        $pkg->setIntlDoc(base64_decode($pkg->getIntlDoc()));
+    }
+
+    $pkg->save();
+}
+
+$installer->endSetup();
