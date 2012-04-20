6a2984abe870b4a51399d493845399074ba9aec7
Kevin Abel <kabel2@unl.edu>
2012-04-20 00:12:07 +0000

    Use the new backup helper in the maintenance notification. (MUST CLEAR CACHE BEFORE ADMIN USE).

--- app/code/local/Unl/Core/Block/Adminhtml/Notification/Maintenance.php
+++ app/code/local/Unl/Core/Block/Adminhtml/Notification/Maintenance.php
@@ -3,13 +3,13 @@
 class Unl_Core_Block_Adminhtml_Notification_Maintenance extends Mage_Adminhtml_Block_Template
 {
     /**
-     * Check if the maintenance file exists (from index.php)
+     * Check if the maintenance file exists
      *
      * @return boolean
      */
     protected function _canShowNotification()
     {
-        global $maintenanceFile;
+        $maintenanceFile = Mage::helper('backup')->isMaintenanceOn();
         return !empty($maintenanceFile) && file_exists($maintenanceFile);
     }
 
