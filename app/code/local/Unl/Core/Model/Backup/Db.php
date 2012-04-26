32b68651c2546cc14c59db824c2f73aced5a9312
Kevin Abel <kabel2@unl.edu>
2012-04-26 18:43:27 +0000

    Ignore the tax calculation and rate tables from the backup. They are backed up elsewhere.

--- app/code/local/Unl/Core/Model/Backup/Db.php
+++ app/code/local/Unl/Core/Model/Backup/Db.php
@@ -4,6 +4,8 @@ class Unl_Core_Model_Backup_Db extends Mage_Backup_Model_Db
 {
     public function __construct()
     {
+        $this->_ignoreDataTablesList[] = 'tax/tax_calculation';
+        $this->_ignoreDataTablesList[] = 'tax/tax_calculation_rate';
         $this->_ignoreDataTablesList[] = 'unl_core/tax_boundary';
     }
 }
