374839d5b9bf27f94e11492ade679175d0624b6b
Kevin Abel <kevin.abel.0@gmail.com>
2012-05-02 09:25:27 -0500

    Fix typo in controller action name for export.

--- app/code/local/Unl/Core/Block/Adminhtml/Report/Sales/Reconcile/Co/Shipping/Paid.php
+++ app/code/local/Unl/Core/Block/Adminhtml/Report/Sales/Reconcile/Co/Shipping/Paid.php
@@ -7,7 +7,7 @@ class Unl_Core_Block_Adminhtml_Report_Sales_Reconcile_Co_Shipping_Paid
     {
         parent::_construct();
         $this->_resourceCollectionName = 'unl_core/report_reconcile_co_shipping_paid';
-        $this->_exportExcelUrl = '*/*/exportExcelCoShippinPaid';
+        $this->_exportExcelUrl = '*/*/exportExcelCoShippingPaid';
         $this->_exportCsvUrl   = '*/*/exportCsvCoShippingPaid';
     }
 
