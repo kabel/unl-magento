ef92e4dcab2402e4d3c722967b5b3f904a234ef6
Kevin Abel <kabel2@unl.edu>
2012-10-09 16:04:09 -0500

    Added a potential download helper override to support HTTPS.

---
+++ app/code/local/Unl/Core/Helper/Downloadable/Download.php
@@ -0,0 +1,43 @@
+<?php
+
+class Unl_Core_Helper_Downloadable_Download extends Mage_Downloadable_Helper_Download
+{
+    protected function _getHandle()
+    {
+        if (!$this->_resourceFile) {
+            Mage::throwException(Mage::helper('downloadable')->__('Please set resource file and link type.'));
+        }
+
+        if (is_null($this->_handle)) {
+            if ($this->_linkType == self::LINK_TYPE_URL) {
+                $client = new Zend_Http_Client($this->_resourceFile, array(
+                    'maxredirects' => 0,
+                    'useragent' => 'Magento ver/' . Mage::getVersion(),
+                    'timeout' => 30,
+                    'storeresponse' => false,
+                    'adapter' => 'Zend_Http_Client_Adapter_Curl',
+                ));
+
+                $client->setStream();
+                $client->setHeaders('Accept-encoding', 'identity');
+
+                try {
+                    /** @var $response Zend_Http_Response_Stream */
+                    $response = $client->request('GET');
+                    $this->_handle = $response->getStream();
+                    $this->_urlHeaders = array_change_key_case($response->getHeaders(), CASE_LOWER);
+
+                    if ($response->getStatus() != 200) {
+                        Mage::throwException(Mage::helper('downloadable')->__('An error occurred while getting the requested content. Please contact the store owner.'));
+                    }
+                } catch (Zend_Http_Client_Exception $e) {
+                    Mage::throwException(Mage::helper('downloadable')->__('An error occurred while getting the requested content. Please contact the store owner.'));
+                }
+            }
+            else {
+                return parent::_getHandle();
+            }
+        }
+        return $this->_handle;
+    }
+}
