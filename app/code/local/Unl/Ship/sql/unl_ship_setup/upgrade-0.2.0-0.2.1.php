788014849a4f275c0cd29f41b2ea0b5cf11bf047
Kevin Abel <kabel2@unl.edu>
2012-12-03 11:08:05 -0600

    Add redundent index to package table to speed up counting

---
+++ app/code/local/Unl/Ship/sql/unl_ship_setup/upgrade-0.2.0-0.2.1.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $installer Unl_Ship_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+
+/**
+ * Add indexes
+ */
+$installer->getConnection()->addIndex(
+    $installer->getTable('unl_ship/shipment_package'),
+    $installer->getIdxName('unl_ship/shipment_package', array('package_id', 'order_id')),
+    array('package_id', 'order_id')
+);
+
+$installer->endSetup();
