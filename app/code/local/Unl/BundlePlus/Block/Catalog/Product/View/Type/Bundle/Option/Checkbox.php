8166ddbeba39060e6963e87953f38f1bc1b93c75
Kevin Abel <kabel2@unl.edu>
2013-02-06 13:39:10 -0600

    Fix checkbox bundle option not showing default qty on default. Also shows qty input for single selection mode.

--- app/code/local/Unl/BundlePlus/Block/Catalog/Product/View/Type/Bundle/Option/Checkbox.php
+++ app/code/local/Unl/BundlePlus/Block/Catalog/Product/View/Type/Bundle/Option/Checkbox.php
@@ -8,6 +8,12 @@ class Unl_BundlePlus_Block_Catalog_Product_View_Type_Bundle_Option_Checkbox
         $this->setTemplate('bundle/catalog/product/view/type/bundle/option/checkbox.phtml');
     }
 
+    /**
+     * Returns the selection qty for a checkbox selection
+     *
+     * @param Mage_Bundle_Model_Selection $selection
+     * @return number
+     */
     protected function _getSelectionQty($selection)
     {
         if ($this->getProduct()->hasPreconfiguredValues()) {
@@ -30,5 +36,25 @@ class Unl_BundlePlus_Block_Catalog_Product_View_Type_Bundle_Option_Checkbox
         }
 
         return $selectedQty;
+    }
+    
+    /**
+     * Gets the default values for a checkbox selection
+     *
+     * @param Mage_Bundle_Model_Selection $selection
+     */
+    protected function _getSelectionDefaultValues($selection)
+    {
+        $selectedOptions = $this->_getSelectedOptions();
+        $_canChangeQty = (bool)$selection->getSelectionCanChangeQty();
+    
+        if ((empty($selectedOptions) && $selection->getIsDefault()) || !$_canChangeQty) {
+            $_defaultQty = $selection->getSelectionQty()*1;
+        } else {
+            $_defaultQty = $this->_getSelectionQty($selection)*1;
+        }
+        
+        
+        return array($_defaultQty, $_canChangeQty);
     }
 }
