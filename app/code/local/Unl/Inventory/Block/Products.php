acaff948dbbcac3874f0d3bedf24c0349663d5fe
Kevin Abel <kabel2@unl.edu>
2013-03-05 15:35:59 -0600

    Do not render an "add" button for inventory management.

--- app/code/local/Unl/Inventory/Block/Products.php
+++ app/code/local/Unl/Inventory/Block/Products.php
@@ -8,5 +8,7 @@ class Unl_Inventory_Block_Products extends Mage_Adminhtml_Block_Widget_Grid_Cont
         $this->_controller = 'products';
         $this->_headerText = Mage::helper('unl_inventory')->__('Manage Product Inventory');
         parent::__construct();
+
+        $this->_removeButton('add');
     }
 }
