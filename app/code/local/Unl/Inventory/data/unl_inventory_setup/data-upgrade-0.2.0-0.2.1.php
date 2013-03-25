98e98b52ca600e29d882954b8f147814dd610be0
Kevin Abel <kabel2@unl.edu>
2013-03-25 16:02:42 -0500

    Ensure that poorly created audit logs are removed on upgrade.

--- app/code/local/Unl/Inventory/data/unl_inventory_setup/data-upgrade-0.2.0-0.2.1.php
+++ app/code/local/Unl/Inventory/data/unl_inventory_setup/data-upgrade-0.2.0-0.2.1.php
@@ -7,3 +7,12 @@ $installer->getConnection()->update($installer->getTable('unl_inventory/purchase
     'qty' => new Zend_Db_Expr($installer->getConnection()->quoteIdentifier('qty_on_hand')),
     'amount' => new Zend_Db_Expr($installer->getConnection()->quoteIdentifier('amount_remaining'))
 ));
+
+$installer->getConnection()->delete($installer->getTable('unl_inventory/audit'), array(
+    'qty = ?' => 0,
+    'amount = ?' => 0,
+    'type IN (?)' => array(
+        Unl_Inventory_Model_Audit::TYPE_SALE,
+        Unl_Inventory_Model_Audit::TYPE_CREDIT,
+    ),
+));
