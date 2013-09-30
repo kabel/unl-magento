18f959b64c10ee43c200d6d6bf8a17557fb1d2ea
Kevin Abel <kabel2@unl.edu>
2013-09-30 17:21:01 -0500

    Add a helper class to handler the logic for download reset visibility

---
+++ app/code/local/Unl/DownloadablePlus/Helper/Data.php
@@ -0,0 +1,24 @@
+<?php
+
+class Unl_DownloadablePlus_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    /**
+     * Returns the URL to reset the use of a download link, or
+     * false if this cannot be done
+     *
+     * @param Mage_Downloadable_Model_Link_Purchased_Item $link
+     * @param Mage_Sales_Model_Order $order
+     */
+    public function getResetDownloadUrl($link, $order)
+    {
+        if ($order->getState() == Mage_Sales_Model_Order::STATE_CLOSED) {
+            return false;
+        }
+
+        if ($link->getNumberOfDownloadsBought() && $link->getStatus() == Mage_Downloadable_Model_Link_Purchased_Item::LINK_STATUS_EXPIRED) {
+            return $this->_getUrl('*/downloadable_link_purchased/resetUse', array('order_id' => $order->getId(), 'item_id' => $link->getId()));
+        }
+
+        return false;
+    }
+}
