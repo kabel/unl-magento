09d231ff55e66aeeec85b8b9697395632ce7d76a
Kevin Abel <kabel2@unl.edu>
2013-10-07 19:19:32 -0500

    Save memory and improve output by avoiding GD to output thumbnail.

--- app/code/local/Unl/Core/controllers/Adminhtml/Cms/Wysiwyg/ImagesController.php
+++ app/code/local/Unl/Core/controllers/Adminhtml/Cms/Wysiwyg/ImagesController.php
@@ -12,9 +12,14 @@ class Unl_Core_Adminhtml_Cms_Wysiwyg_ImagesController extends Mage_Adminhtml_Cms
         $file = Mage::helper('cms/wysiwyg_images')->idDecode($file);
         $thumb = $this->getStorage()->resizeOnTheFly($file);
         if ($thumb !== false) {
-            $image = Varien_Image_Adapter::factory('GD2');
-            $image->open($thumb);
-            $this->getResponse()->setHeader('Content-type', $image->getMimeType())->setBody($image->getImage());
+            $size = getimagesize($thumb);
+            $this->getResponse()
+                ->setHeader('Content-type', $size['mime'])
+                ->clearBody();
+
+            $this->getResponse()->sendHeaders();
+            readfile($thumb);
+            exit;
         } else {
             // todo: genearte some placeholder
         }
