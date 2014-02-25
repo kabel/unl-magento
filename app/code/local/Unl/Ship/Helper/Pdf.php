afd2f33a1698d9cb61ecd29281a0609ce71b8732
Kevin Abel <kabel2@unl.edu>
2014-02-25 17:17:39 -0600

    Use constants for PDF label size.

--- app/code/local/Unl/Ship/Helper/Pdf.php
+++ app/code/local/Unl/Ship/Helper/Pdf.php
@@ -2,22 +2,22 @@
 
 class Unl_Ship_Helper_Pdf extends Mage_Core_Helper_Abstract
 {
+    //label size in a pts (1/72 inch) - 4" x 6"
+    const LABEL_4X6_WIDTH = 288;
+    const LABEL_4X6_HEIGHT = 432;
+
     public function attachImagePage(Zend_Pdf $pdf, Zend_Pdf_Resource_Image $pdfimg)
     {
-        //label size in a pts (1/72 inch) - 6" x 4"
-        $label4x6landscape = '432:288:';
-        $label4x6portrait = '288:432:';
-
-        $pdfpage = $pdf->newPage($label4x6portrait);
+        $pdfpage = $pdf->newPage(self::LABEL_4X6_WIDTH, self::LABEL_4X6_HEIGHT);
 
         $width = $pdfimg->getPixelWidth();
         $height = $pdfimg->getPixelHeight();
 
         // scale the image to proper width
-        $ratio = 288 / $width;
+        $ratio = self::LABEL_4X6_WIDTH / $width;
         $width = $width * $ratio;
         $height = $height * $ratio;
-        $offset = 432 - $height;
+        $offset = self::LABEL_4X6_HEIGHT - $height;
 
         //get the image into a PdfImage object
         $pdfpage->drawImage($pdfimg, 0, $offset, $width, $height + $offset);
