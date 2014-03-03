c5cbe49d776182d514896fc9d60aaefbfc9d9a5b
Kevin Abel <kabel2@unl.edu>
2014-03-03 16:35:50 -0600

    Make the Price block override compatible with the parent.

--- app/code/local/Unl/Core/Block/Catalog/Product/Price.php
+++ app/code/local/Unl/Core/Block/Catalog/Product/Price.php
@@ -2,12 +2,18 @@
 
 class Unl_Core_Block_Catalog_Product_Price extends Mage_Catalog_Block_Product_Price
 {
-    public function getTierPrices($product = null)
+    public function getTierPrices($product = null, $parent = null)
     {
         if (is_null($product)) {
             $product = $this->getProduct();
         }
         $prices = $product->getFormatedTierPrice();
+
+        // if our parent is a bundle, then we need to further adjust our tier prices
+        if (isset($parent) && $parent->getTypeId() == Mage_Catalog_Model_Product_Type::TYPE_BUNDLE) {
+            /* @var $bundlePriceModel Mage_Bundle_Model_Product_Price */
+            $bundlePriceModel = Mage::getModel('bundle/product_price');
+        }
 
         $res = array();
         $i = 0;
@@ -27,7 +33,14 @@ class Unl_Core_Block_Catalog_Product_Price extends Mage_Catalog_Block_Product_Pr
                 }
 
                 if ($price['price'] < $productPrice) {
+                    // use the original prices to determine the percent savings
                     $price['savePercent'] = ceil(100 - ((100 / $productPrice) * $price['price']));
+
+                    // if applicable, adjust the tier prices
+                    if (isset($bundlePriceModel)) {
+                        $price['price']         = $bundlePriceModel->getLowestPrice($parent, $price['price']);
+                        $price['website_price'] = $bundlePriceModel->getLowestPrice($parent, $price['website_price']);
+                    }
 
                     $tierPrice = Mage::app()->getStore()->convertPrice(
                         Mage::helper('tax')->getPrice($product, $price['website_price'])
