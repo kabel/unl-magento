39b8da8fd4540b4884ba422d77e699cb1829ecac
Kevin Abel <kabel2@unl.edu>
2014-06-30 14:10:13 -0500

    Override Magento product option render class to better support a11y

---
+++ app/code/local/Unl/Core/Block/Catalog/Product/View/Options/Type/Select.php
@@ -0,0 +1,118 @@
+<?php
+
+class Unl_Core_Block_Catalog_Product_View_Options_Type_Select
+    extends Mage_Catalog_Block_Product_View_Options_Type_Select
+{
+    public function getValuesHtml()
+    {
+        $_option = $this->getOption();
+        $configValue = $this->getProduct()->getPreconfiguredValues()->getData('options/' . $_option->getId());
+        $store = $this->getProduct()->getStore();
+
+        if ($_option->getType() == Mage_Catalog_Model_Product_Option::OPTION_TYPE_DROP_DOWN
+            || $_option->getType() == Mage_Catalog_Model_Product_Option::OPTION_TYPE_MULTIPLE
+        ) {
+            $require = ($_option->getIsRequire()) ? ' required-entry' : '';
+            $extraParams = '';
+            $select = $this->getLayout()->createBlock('core/html_select')
+                ->setData(array(
+                    'id' => 'select_'.$_option->getId(),
+                    'class' => $require.' product-custom-option'
+                ));
+            if ($_option->getType() == Mage_Catalog_Model_Product_Option::OPTION_TYPE_DROP_DOWN) {
+                $optParams = array();
+                if (empty($configValue)) {
+                    $optParams['selected'] = 'selected';
+                }
+                $select->setName('options['.$_option->getid().']')
+                    ->addOption('', $this->__('-- Please Select --'), $optParams);
+            } else {
+                $select->setName('options['.$_option->getid().'][]');
+                $select->setClass('multiselect'.$require.' product-custom-option');
+            }
+            foreach ($_option->getValues() as $_value) {
+                $priceStr = $this->_formatPrice(array(
+                    'is_percent'    => ($_value->getPriceType() == 'percent'),
+                    'pricing_value' => $_value->getPrice(($_value->getPriceType() == 'percent'))
+                ), false);
+                $select->addOption(
+                    $_value->getOptionTypeId(),
+                    $_value->getTitle() . ' ' . $priceStr . ''
+                );
+            }
+            if ($_option->getType() == Mage_Catalog_Model_Product_Option::OPTION_TYPE_MULTIPLE) {
+                $extraParams = ' multiple="multiple"';
+            }
+            if (!$this->getSkipJsReloadPrice()) {
+                $extraParams .= ' onchange="opConfig.reloadPrice()"';
+            }
+            $select->setExtraParams($extraParams);
+
+            if ($configValue) {
+                $select->setValue($configValue);
+            }
+
+            return $select->getHtml();
+        }
+
+        if ($_option->getType() == Mage_Catalog_Model_Product_Option::OPTION_TYPE_RADIO
+            || $_option->getType() == Mage_Catalog_Model_Product_Option::OPTION_TYPE_CHECKBOX
+        ) {
+            $selectHtml = '<ul id="options-'.$_option->getId().'-list" class="options-list">';
+            $require = ($_option->getIsRequire()) ? ' validate-one-required-by-name' : '';
+            $arraySign = '';
+            switch ($_option->getType()) {
+                case Mage_Catalog_Model_Product_Option::OPTION_TYPE_RADIO:
+                    $type = 'radio';
+                    $class = 'radio';
+                    if (!$_option->getIsRequire()) {
+                        $selectHtml .= '<li><input type="radio" id="options_' . $_option->getId() . '" class="'
+                            . $class . ' product-custom-option" name="options[' . $_option->getId() . ']"'
+                            . ($this->getSkipJsReloadPrice() ? '' : ' onclick="opConfig.reloadPrice()"')
+                            . ' value="" checked="checked" /><span class="label"><label for="options_'
+                            . $_option->getId() . '">' . $this->__('None') . '</label></span></li>';
+                    }
+                    break;
+                case Mage_Catalog_Model_Product_Option::OPTION_TYPE_CHECKBOX:
+                    $type = 'checkbox';
+                    $class = 'checkbox';
+                    $arraySign = '[]';
+                    break;
+            }
+            $count = 1;
+            foreach ($_option->getValues() as $_value) {
+                $count++;
+
+                $priceStr = $this->_formatPrice(array(
+                    'is_percent'    => ($_value->getPriceType() == 'percent'),
+                    'pricing_value' => $_value->getPrice($_value->getPriceType() == 'percent')
+                ));
+
+                $htmlValue = $_value->getOptionTypeId();
+                if ($arraySign) {
+                    $checked = (is_array($configValue) && in_array($htmlValue, $configValue)) ? 'checked' : '';
+                } else {
+                    $checked = $configValue == $htmlValue ? 'checked' : '';
+                }
+
+                $selectHtml .= '<li>' . '<input type="' . $type . '" class="' . $class . ' ' . $require
+                    . ' product-custom-option"'
+                    . ($this->getSkipJsReloadPrice() ? '' : ' onclick="opConfig.reloadPrice()"')
+                    . ' name="options[' . $_option->getId() . ']' . $arraySign . '" id="options_' . $_option->getId()
+                    . '_' . $count . '" value="' . $htmlValue . '" ' . $checked . ' />'
+                    . '<span class="label"><label for="options_' . $_option->getId() . '_' . $count . '">'
+                    . $_value->getTitle() . ' ' . $priceStr . '</label></span>';
+                if ($_option->getIsRequire()) {
+                    $selectHtml .= '<script type="text/javascript">' . '$(\'options_' . $_option->getId() . '_'
+                        . $count . '\').advaiceContainer = \'options-' . $_option->getId() . '-container\';'
+                        . '$(\'options_' . $_option->getId() . '_' . $count
+                        . '\').callbackFunction = \'validateOptionsCallback\';' . '</script>';
+                }
+                $selectHtml .= '</li>';
+            }
+            $selectHtml .= '</ul>';
+
+            return $selectHtml;
+        }
+    }
+}
