a6e041f1438e90ab9ccb57a811b97bd03d29c1b8
Kevin Abel <kabel2@unl.edu>
2014-08-14 12:02:05 -0500

    Add an install script to DownloadablePlus module
    To support adding a DB field to hold callback hook shared secret values.
    This value will be used to generate HMAC values.

---
+++ app/code/local/Unl/DownloadablePlus/sql/unl_downloadableplus_setup/install-0.2.0.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $installer Mage_Catalog_Model_Resource_Eav_Mysql4_Setup */
+$installer = $this;
+
+$installer->getConnection()->addColumn($installer->getTable('downloadable/link'), 'link_secret', array(
+    'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+    'size' => 255,
+    'comment' => 'String used to generate callback signature via HMAC',
+    'after' => 'link_url'
+));
+
+$installer->getConnection()->addColumn($installer->getTable('downloadable/link_purchased_item'), 'link_secret', array(
+    'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+    'size' => 255,
+    'comment' => 'String used to generate callback signature via HMAC',
+    'after' => 'link_url'
+));
